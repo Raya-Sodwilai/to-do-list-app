65dca8421002aaa99caa4a1bc104ad797374808b
Raya Sodwilai <sodwilai@pdx.edu>
2021-04-19 15:03:10 -0700

    full testing with C#

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public string Description { get; set; }
+    private static List<Item> _instance = new List<Item> {};
+
+    public Item(string description)
+    {
+      Description = description;
+      _instance.Add(this);
+    }
+
+    public static List<Item> GetAll()
+    {
+      return _instance;
+    }
+
+    public static void ClearAll()
+    {
+      _instance.Clear();
+    }
+  }
+}
